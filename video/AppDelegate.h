d1359669064ca728eb5a99d9f9cafe5fb1e80c7e
chenyingchao <893088206@qq.com>
2019-08-07 18:29:58 +0800

    Initial Commit

---
+++ video/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  video
+//
+//  Created by butter on 2019/8/7.
+//  Copyright © 2019 butter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ video/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  video
+//
+//  Created by butter on 2019/8/7.
+//  Copyright © 2019 butter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
